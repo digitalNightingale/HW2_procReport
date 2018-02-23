f0db9f7834e033dbb426b54ea03930ddab93245a
digitalNightingale <lmr81@uw.edu>
2018-02-23 12:58:19 -0800

    added mor printk

--- procReport.c
+++ procReport.c
@@ -3,56 +3,80 @@
 #include <linux/sched.h>
 #include <linux/init.h>
 
-
 int unrun = 0;
 int run = 0;
 int stop = 0;
 
 void getRunables(struct task_struct *currentTask) {    // https://notes.shichao.io/lkd/ch3/
-    struct task_struct *parentTaskPtr;  // pointer to parent task
-    struct list_head *childList;        // list of children
+    struct task_struct *parentTask;                    // pointer to parent task
+    struct list_head *childList;                       // list of children
 
-    if (currentTask->state < 0) {       // state == -1 is unrunnable
-      unrun++;
+    if (currentTask->state < 0) {                      // state == -1 is unrunnable
+        unrun++;
+    } else if (currentTask->state == 0) {              // state == 0 is runnable
+        run++;
+    } else {                                           // state > 0 is stopped
+        stop++;
     }
-    else if (currentTask->state == 0) { // state == 0 is runnable
-      run++;
-  } else { // state > 0 is stopped
-      stop++;
-    }
+    // go through the current task list and count states recursivly
     list_for_each(childList, &currentTask->children) {
-        parentTaskPtr = list_entry(childList, struct task_struct, sibling);
-        getRunables(parentTaskPtr);
+        parentTask = list_entry(childList, struct task_struct, sibling);
+        getRunables(parentTask);
     }
 }
 
-void printProcesses(struct task_struct *currentTask) {     // source: https://linuxgazette.net/133/saha.html
+void printProcesses(struct task_struct *parentTask) {     // source: https://linuxgazette.net/133/saha.html
 
+    struct task_struct *childTask;
+    struct list_head *childList;
+    int children = 0;
 
-
-
-  for_each_process(currentTask) {
-      //printk(KERN_INFO "Process ID=%d\n", task->pid);
-      //printk(KERN_INFO "Name=%s\n", task->comm);
-      //printk(KERN_INFO "State of process=%ld]\n", task->state);
-      printk(KERN_INFO "Process ID=%d Name=%s number_of_children=*not coded* **_child_pid=*not coded* **_child_name=*not coded*\n", currentTask->pid, currentTask->comm);
-  }
-
+    //for_each_process(parentTask) {
+        //printk(KERN_INFO "Process ID=%d\n", parentTask->pid);
+        //printk(KERN_INFO "Name=%s\n", parentTask->comm);
+        //printk(KERN_INFO "State of process=%ld]\n", parentTask->state);
+        list_for_each(childList, &parentTask->children) {
+            children++;
+        }
+        // if there are no children processes print no children
+        if (children == 0) {
+            printk(KERN_INFO "Process ID=%d Name=%s *No Children", parentTask->pid, parentTask->comm);
+        // else print out the number of children and the first child process
+        } else {
+            childTask = list_first_entry(childList, struct task_struct, sibling);
+            printk("Process ID=%d Name=%s number_of_children=%d first_child_pid=%d first_child_name=%s", parentTask->pid, parentTask->comm, children, childTask->pid, childTask->comm);
+        }
+        // recursivly go through the parent list to find each parent/child combo
+        list_for_each(childList, &parentTask->children) {
+            childTask = list_entry(childList, struct task_struct, sibling);
+            printProcesses(childTask);
+        }
+        // printk(KERN_INFO "Process ID=%d Name=%s number_of_children=*not coded* **_child_pid=*not coded* **_child_name=*not coded*\n", parentTask->pid, parentTask->comm);
+    //}
 }
 
+/**
+ * This function is called when the module is loaded.
+ *
+ * @return 0  upon success
+ */
 int proc_init (void) {
-  printk(KERN_INFO "helloModule: kernel module initialized\n");
-  printk(KERN_INFO "PROCESS REPORTER\n");
-  getRunables(&init_task);
-  printk(KERN_INFO "Unrunnable:%d\nRunnable:%d\nStopped:%d\n", unrun, run, stop);
-  printProcesses(&init_task);
-  return 0;
+    printk(KERN_INFO "helloModule: kernel module initialized\n");
+    printk(KERN_INFO "PROCESS REPORTER\n");
+    getRunables(&init_task);
+    printk(KERN_INFO "Unrunnable:%d\nRunnable:%d\nStopped:%d\n", unrun, run, stop);
+    printProcesses(&init_task);
+    return 0;
 }
 
+/**
+ * This function is called when the module is removed.
+ */
 void proc_cleanup(void) {
-  printk(KERN_INFO "helloModule: performing cleanup of module\n");
+    printk(KERN_INFO "helloModule: performing cleanup of module\n");
 }
 
+// Macros for registering module entry and exit points.
 MODULE_LICENSE("GPL");
 module_init(proc_init);
 module_exit(proc_cleanup);
